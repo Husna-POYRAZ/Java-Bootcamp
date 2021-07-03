7ee4b328ebba6907546a3661fb28b80a6cfdc804
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2021-07-04 01:31:41 +0300

    Java Enterprise Architecture

---
+++ week5/nLayeredDemo/src/nLayeredDemo/Main.java
@@ -0,0 +1,23 @@
+package nLayeredDemo;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
+import nLayeredDemo.dataAccsess.concretes.AbcProductDao;
+import nLayeredDemo.dataAccsess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//ToDo; Spring IOC ile çözülecek
+		ProductService productService = new ProductManager(new AbcProductDao()
+				                        ,new JLoggerManagerAdapter());
+        
+		Product product = new Product(1, 2, "Elma", 12, 50);
+		productService.add(product);
+		
+	}
+
+}
