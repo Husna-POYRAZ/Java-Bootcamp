b2bceeb3d360a1417e988f723917e27f868ab7f4
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2021-07-05 22:00:14 +0300

    Spring Boot in Enterprise Architecture

---
+++ week6/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,94 @@
+package kodlamaio.northwind.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// annotation
+@Entity
+@Table(name="products")
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="product_id")
+	private int id;
+	
+	@Column(name="category_id")
+	private int categoryid;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	@Column(name="unit_price")
+	private double unitPrice;
+	
+	@Column(name="units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name="quantity_per_unit")
+	private String quantityPerUnit;
+	
+	public Product() {}
+	
+	public Product(int id, int categoryid, String productName, double unitPrice, short unitsInStock,
+			String quantityPerUnit) {
+		super();
+		this.id = id;
+		this.categoryid = categoryid;
+		this.productName = productName;
+		this.unitPrice = unitPrice;
+		this.unitsInStock = unitsInStock;
+		this.quantityPerUnit = quantityPerUnit;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCategoryid() {
+		return categoryid;
+	}
+
+	public void setCategoryid(int categoryid) {
+		this.categoryid = categoryid;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public short getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(short unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getQuantityPerUnit() {
+		return quantityPerUnit;
+	}
+
+	public void setQuantityPerUnit(String quantityPerUnit) {
+		this.quantityPerUnit = quantityPerUnit;
+	}
+}
